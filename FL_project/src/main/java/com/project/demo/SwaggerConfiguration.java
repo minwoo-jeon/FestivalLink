7ae2e4997b2f5c4ac40baa9af32a4cdbd75abd56
kkh97 <kgh970809@naver.com>
2023-06-16 14:17:16 +0900

    return ModelAndView("test")

--- FL_project/src/main/java/com/project/demo/SwaggerConfiguration.java
+++ FL_project/src/main/java/com/project/demo/SwaggerConfiguration.java
@@ -1,5 +1,6 @@
 
 package com.project.demo;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -59,6 +60,7 @@ import io.swagger.v3.oas.models.info.Info;
 //	}
 //
 //}
+
 @Configuration
 public class SwaggerConfiguration {
 
