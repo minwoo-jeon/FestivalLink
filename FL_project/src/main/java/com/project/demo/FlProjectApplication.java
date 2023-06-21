99fa7053792c25c8e0a84b187b501c9b1504331d
1013yjkim <1013yjkim@naver.com>
2023-06-21 13:50:06 +0900

    [Fix] FlProjectApplication MapperScan설정

--- FL_project/src/main/java/com/project/demo/FlProjectApplication.java
+++ FL_project/src/main/java/com/project/demo/FlProjectApplication.java
@@ -1,10 +1,12 @@
 package com.project.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+@MapperScan(basePackages= {"com.project.mapper"})
 @SpringBootApplication(exclude= {SecurityAutoConfiguration.class})
 @ComponentScan(basePackages= {"com.project"})
 public class FlProjectApplication {
