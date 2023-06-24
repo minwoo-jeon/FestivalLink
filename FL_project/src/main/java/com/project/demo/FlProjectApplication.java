aef766743c6fb77df661792c22ba790a84c22ad2
1013yjkim <1013yjkim@naver.com>
2023-06-24 11:50:01 +0900

    신경x
    매퍼 위치 바꾼거

--- FL_project/src/main/java/com/project/demo/FlProjectApplication.java
+++ FL_project/src/main/java/com/project/demo/FlProjectApplication.java
@@ -3,11 +3,14 @@ package com.project.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@MapperScan(basePackages= {"com.project.mapper"})
+
 @SpringBootApplication(exclude= {SecurityAutoConfiguration.class})
+@EntityScan("com.project.domain")
+@MapperScan(basePackages= {"com.project.mapper"})
 @ComponentScan(basePackages= {"com.project"})
 public class FlProjectApplication {
 
