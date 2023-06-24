0488d996cea716a12d0d9269e6d198bd30ef4973
1013yjkim <1013yjkim@naver.com>
2023-06-24 11:53:02 +0900

    [fix]NaverOauthTokenVO 스웨거 어노테이션 추가

--- FL_project/src/main/java/com/project/domain/NaverOauthTokenVO.java
+++ FL_project/src/main/java/com/project/domain/NaverOauthTokenVO.java
@@ -1,10 +1,13 @@
 package com.project.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class NaverOauthTokenVO {
+	@Schema(description = "엑세스 토큰")
     private String access_token; // 접근 토큰, 발급 후 expires_in 파라미터에 설정된 시간(초)이 지나면 만료됨
+	@Schema(description = "리프레시 토큰")
 	private String refresh_token; // 갱신 토큰, 접근 토큰이 만료될 경우 접근 토큰을 다시 발급받을 때 사용
 	private String token_type; // 접근 토큰의 타입으로 Bearer와 MAC의 두 가지를 지원
 	private Integer expires_in; // 접근 토큰의 유효 기간(초 단위)
