0c6c9c9fae4dd87c3302eefabab34a461c5232d4
1013yjkim <1013yjkim@naver.com>
2023-07-02 12:07:05 +0900

    [Add]#16,17,45 mapper

--- FL_project/src/main/java/com/project/mapper/LikeOrUnlikeMapper.java
+++ FL_project/src/main/java/com/project/mapper/LikeOrUnlikeMapper.java
@@ -1,11 +1,24 @@
 package com.project.mapper;
 
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.project.domain.F_likesVO;
 import com.project.domain.R_likesVO;
-
+@Mapper
 public interface LikeOrUnlikeMapper {
+    // 리뷰 좋아요
     public void likeReview(R_likesVO vo);
+    //리뷰 좋아요 취소
     public void unlikeReview(@Param("userId") String userId, @Param("reviewId") String reviewId);
+    //축제 좋아요 확인
+    public int didLikeFestival(@Param("userId") String userId, @Param("festivalId") String festivalId);
+    //축제 좋아요
+    public void likeFestival(F_likesVO vo);
+    //축제 좋아요 취소
+    public void delLikeFestival(@Param("userId") String userId, @Param("festivalId") String festivalId);
+    //리뷰에 좋아요 했는지 안했는지
+    public int didLikeReview(String userId, String reviewId);
+
 }
