d6d3f5f7e5adede550e1639d32f484b854ddad9f
1013yjkim <1013yjkim@naver.com>
2023-07-02 12:06:08 +0900

    [Fix]#18,#19URI [ADD]#45,#16,#17

--- FL_project/src/main/java/com/project/controller/LikeOrUnlikeController.java
+++ FL_project/src/main/java/com/project/controller/LikeOrUnlikeController.java
@@ -5,10 +5,13 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.domain.F_likesVO;
 import com.project.domain.R_likesVO;
 import com.project.mapper.LikeOrUnlikeMapper;
 
@@ -20,16 +23,17 @@ import java.util.UUID;
 @Slf4j
 @RestController
 public class LikeOrUnlikeController {
+    
    @Autowired
     private LikeOrUnlikeMapper likeOrUnlikeMapper;
 
 
-    @Operation(summary = "내가 쓴 리뷰") // 정의하려는 API 명시
+    @Operation(summary = "리뷰에 좋아요") // 정의하려는 API 명시
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "내가 쓴 리뷰 리스트 찾아서 모델에 반환 ")
+            @ApiResponse(responseCode = "200", description = "리뷰에 좋아요 누르면 R_likesVO만들어서 DB에 insert ")
     })
     @PostMapping("/users/like")
-    public int likeReview(HttpServletRequest req, @RequestParam String reviewId) {
+    public int likeReview(HttpServletRequest req, @RequestParam(name="reviewId", required = true) String reviewId) {
         HttpSession session = req.getSession();
         // 세션에서 유저아이디 가져오기
         String userId = (String) session.getAttribute("userId");
@@ -39,8 +43,12 @@ public class LikeOrUnlikeController {
         likeOrUnlikeMapper.likeReview(vo);
         return 1;
     }
+    @Operation(summary = "리뷰에 좋아요 취소") // 정의하려는 API 명시
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "리뷰에 좋아요 다시 누르면 reviewId userId같은 좋아요 찾아 삭제")
+    })
     @DeleteMapping("/users/like")
-    public int unlikeReview(HttpServletRequest req, @RequestParam String reviewId){
+    public int unlikeReview(HttpServletRequest req, @RequestParam(name="reviewId", required = true) String reviewId){
         HttpSession session = req.getSession();
         // 세션에서 유저아이디 가져오기
         String userId = (String) session.getAttribute("userId");
@@ -48,4 +56,60 @@ public class LikeOrUnlikeController {
         return 1;
     }
 
-}
+    @Operation(summary = "축제에 좋아요 했는지 확인 ") // 정의하려는 API 명시
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "축제상세 로딩 시 로그인 중이면 내가 좋아요 했는지 아닌지 확인 ")
+    })
+    @GetMapping("/users/like/festival/{festivalId}")
+    public int didLikeFestival(HttpServletRequest req, @PathVariable String festivalId){
+        HttpSession session = req.getSession();
+        // 세션에서 유저아이디 가져오기
+        String userId = (String) session.getAttribute("userId");
+        int res = likeOrUnlikeMapper.didLikeFestival(userId, festivalId);
+        if(res>=1) res = 1;
+        
+        return res;
+    }
+    @Operation(summary = "축제에 좋아요") // 정의하려는 API 명시
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "축제에 좋아요 누르면 F_likesVO만들어서 DB에 insert ")
+    })
+    @PostMapping("/users/like/festival/{festivalId}")
+    public int LikeFestival(HttpServletRequest req, @PathVariable String festivalId){
+        HttpSession session = req.getSession();
+        String userId = (String) session.getAttribute("userId");
+        String f_like_id = UUID.randomUUID().toString();
+        F_likesVO vo = new F_likesVO(f_like_id, userId, festivalId);
+        likeOrUnlikeMapper.likeFestival(vo);
+        return 1;
+    }
+
+    @Operation(summary = "축제에 좋아요 취소") // 정의하려는 API 명시
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "축제에 좋아요 다시 누르면 festivalId userId같은 좋아요 찾아 삭제")
+    })
+    @DeleteMapping("/users/like/festival/{festivalId}")
+    public int unlikeFestival(HttpServletRequest req, @PathVariable String festivalId){
+        HttpSession session = req.getSession();
+        // 세션에서 유저아이디 가져오기
+        String userId = (String) session.getAttribute("userId");
+        likeOrUnlikeMapper.delLikeFestival(userId, festivalId);
+        return 1;
+    }
+
+    @Operation(summary = "축제페이지에서 리뷰에 좋아요 했는지 확인 ") // 정의하려는 API 명시
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "축제상세 로딩 시 로그인 중이면 내가 좋아요 했는지 아닌지 확인 ")
+    })
+    @GetMapping("/users/like/review")
+    public int didLikeReview(HttpServletRequest req, @RequestParam(name="reviewId", required = true) String reviewId){
+        HttpSession session = req.getSession();
+        // 세션에서 유저아이디 가져오기
+        String userId = (String) session.getAttribute("userId");
+        int res = likeOrUnlikeMapper.didLikeReview(userId, reviewId);
+        if(res>=1) res = 1;
+        
+        return res;
+    }
+    
+}   
