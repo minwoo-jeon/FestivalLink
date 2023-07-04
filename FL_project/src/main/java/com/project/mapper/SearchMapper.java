859a1d1f8cd67cc4a2b4f75d21772b93f7fbaec9
gghj20353 <gghj20353@gmail.com>
2023-07-04 13:26:37 +0900

    검색기능추가

--- FL_project/src/main/java/com/project/mapper/SearchMapper.java
+++ FL_project/src/main/java/com/project/mapper/SearchMapper.java
@@ -7,7 +7,7 @@ import com.project.domain.SearchVO;
 
 @Mapper
 public interface SearchMapper {
-	List<SearchVO> getSearchList();
+	List<SearchVO> getSearchList(String keyword);
 	List<SearchVO> getSearchListPaging(PaginationVO vo);
 	int inserSearch(String FESTIVAL_ID,String FESTIVAL_CONTENTS,String FESTIVAL_NAME,String FESTIVAL_START,String FESTIVAL_END,
 			String FESTIVAL_HOST,String FESTIVAL_PLACE,String FESTIVAL_ADDR,String FESTIVAL_LATITUDE,
@@ -19,5 +19,5 @@ public interface SearchMapper {
 			String FESTIVAL_HOST,String FESTIVAL_PLACE,String FESTIVAL_ADDR,String FESTIVAL_LATITUDE,
 			String FESTIVAL_LONGITUDE,String FESTIVAL_HOMEPAGE,String FESTIVAL_IMAGE);
 	int getTotalCount();
-	SearchVO getSearch(String FESTIVAL_id);
+	SearchVO getSearch(String FESTIVAL_NAME);
 	}
